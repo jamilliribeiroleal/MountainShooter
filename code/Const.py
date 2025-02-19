8b3b279394328a66c82208c0764970568473b69b
Jamilli <jamillileal8@gmail.com>
2025-02-19 10:53:01 -0300

    creating menu interactions creating abc class creating factory level 1 bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -2,6 +2,7 @@ from pygame.examples.grid import WINDOW_WIDTH
 #C
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255,255,255)
+COLOR_YELLOW = (255,255,0)
 
 #M
 MENU_OPTION = ('NEW GAME 1P',
@@ -12,3 +13,14 @@ MENU_OPTION = ('NEW GAME 1P',
 #W
 WINDOW_WIDTH = 576
 WINDOW_HEIGHT = 324
+
+
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+}
